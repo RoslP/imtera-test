9e6451f58113bc1bdb1eda662f8c37b9bd05073b
pavel99 <ght12345ght12345@gmail.com>
2025-11-16 15:21:53 +1000

    Вывод спаршенных отзывов

--- resources/js/Pages/Profile/Reviews/Reviews.vue
+++ resources/js/Pages/Profile/Reviews/Reviews.vue
@@ -2,7 +2,12 @@
 
 import {onMounted, ref} from "vue";
 
-const items = ref([])
+const items = ref({
+    raining: String,
+    reviews_count: null,
+    reviews: Array,
+    ref: String,
+})
 
 onMounted(async () => {
     const response = await fetch('/api/get-yandex-reviews')
@@ -11,7 +16,39 @@ onMounted(async () => {
 </script>
 
 <template>
-
+    <div class="ml-[25px] mt-[17px]">
+        <a :href="items.ref">Яндекс карты</a>
+    </div>
+    <div class="reviews_grid_container ml-[25px]">
+        <div>
+            <div class="bg-white drop-shadow-[0_3px_6px_rgba(21,101,111,0.3)] my-4 pl-[15px] pb-[1px] pt-[13px] rounded-xl"
+                 v-for="(value, key) in items.reviews"
+                 :key="key">
+                <div class="mb-[20px] h-[155] min-h-[100px] bg-[#F6F8FA] rounded-xl ">
+                    <div>
+                        {{ value.date }}
+                    </div>
+                    <div>
+                        {{ value.author }}
+                    </div>
+                    <div class="">
+                        {{ value.text }}
+                    </div>
+                    <div>
+                        {{ value.rating }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                {{ items.rating }}
+            </div>
+            <div>
+                Всего отзывов: {{ items.reviews_count }}
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
